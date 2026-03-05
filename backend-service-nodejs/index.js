8bab3adcac98e0d9ec1190d42e127fa5bac6df25
SarahTWLin <sarahtwlin@gmail.com>
2026-03-06 00:36:47 +0800

    Add task router to server

--- backend-service-nodejs/index.js
+++ backend-service-nodejs/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Routes
+const taskRouter = require("./routes/tasks");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use("/tasks/v1/", taskRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
