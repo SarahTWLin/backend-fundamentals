ccaf914bbc2799f825ce5e79ebb897bcbe5000d0
Barahmon <sarahtwlin@gmail.com>
2026-07-15 23:15:34 +0800

    feat(database): Add healthcheck API to postgres database

--- backend-service-nodejs/routes/healthcheck.js
+++ backend-service-nodejs/routes/healthcheck.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const healthcheckRouter = express.Router();
+const pool = require("../database/db");
 
-healthcheckRouter.get('/', (req, res, next) => {
+
+healthcheckRouter.get('/api', (req, res, next) => {
     try {
         res.status(200).send({
             status: "OK"
@@ -9,6 +11,21 @@ healthcheckRouter.get('/', (req, res, next) => {
     }
     catch (err) {
         next(err); 
+    }
+});
+
+healthcheckRouter.get('/db', async (req, res, next) => {
+    try {
+        const result = await pool.query('SELECT NOW();'); 
+        console.log("HE::");
+        
+        res.status(200).send({
+            status: "OK",
+            timestamp: result.rows[0].now
+        });
+    }
+    catch(err) {
+        next(err);
     }
 });
 
